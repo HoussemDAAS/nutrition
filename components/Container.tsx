c47b9259af8042c52218dd84ede54e5ea80813b5
Daas Houssem <houssemdaas2@gmail.com>
2025-01-21 15:07:17 +0100

    Add header, footer, and container components; update layout and Tailwind configuration

---
+++ components/Container.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/lib/utils';
+
+interface props {
+    children:React.ReactNode;
+    className?:string;
+}
+const Container = ({children,className}:props) => {
+  return (
+    <div className={cn('max-w-screen-xl mx-auto px-4',className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Container
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,16 @@ export default {
   theme: {
   	extend: {
   		colors: {
+			// Our colors start
+darkColor: '#151515',
+lightColor: '#52525b',
+lightOrange: '#fca99b',
+lightBlue: '#7688DB',
+darkBlue: '#6c7fd8',
+darkText: '#686e7d',
+lightBg: '#F8F8FB',
+// Our colors end
+
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
